155bb647b32a644f2734d18653ca094b55e2edee
Johann <johann@immedia.co.za>
2013-03-19 11:11:07 +0200

    Implemented creating tickets for a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,8 @@
 class ProjectsController < ApplicationController
+	before_filter :find_project, :only => [:show,
+										   :edit,
+											:update,
+											:destroy]
 	
 	def create
 		@project = Project.new(params[:project])
@@ -32,5 +36,27 @@ class ProjectsController < ApplicationController
 	else
 		flash[:alert] = "Project has not been updated"
 		render :action => "edit"
+	end
+
+
+	def destroy
+		@project = Project.find(params[:id])
+		if @project.destroy
+			flash[:notice] = "Project has been deleted"
+			redirect_to projects_path
+		else
+			flash[:notice] = "Project has not been deleted"
+		end
+	end
+
+
+	private
+		def find_project
+			@project = Project.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			flash[:alert] = "The project you were looking" + " for could not be found"
+			redirect_to projects_path
+
+		end
 	end
 end
